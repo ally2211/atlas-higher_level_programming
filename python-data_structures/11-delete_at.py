793ce6f43573f380d0c6ef09d5ffc2cd5565453b
ally2211 <amendieta.va@gmail.com>
2024-01-05 01:44:14 -0600

    not

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -3,7 +3,7 @@
 def delete_at(my_list=[], idx=0):
     if not my_list or idx < 0:
         return my_list
-    
+
     if my_list and 0 <= idx < len(my_list):
         del my_list[idx]
     return my_list
