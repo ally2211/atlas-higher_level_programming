a097921f923f437ed05949cb2837479f486ceb32
ally2211 <amendieta.va@gmail.com>
2024-01-04 21:04:09 -0600

    add none

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,8 @@
 
 
 def print_reversed_list_integer(my_list=[]):
+    if my_list is None:
+        return
     lenlist = len(my_list)
     for i in range(lenlist, 0, -1):
         print("{:d}".format(my_list[i-1]))
