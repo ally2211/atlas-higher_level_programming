5e6b0787e9d4fb598c814774b129af976c67ab92
ally2211 <amendieta.va@gmail.com>
2024-01-04 22:08:07 -0600

    blank

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -6,6 +6,9 @@ def multiple_returns(sentence):
     if sentence is None:
         length = 0
         first = None
+    elif sentence == "":
+        length = 0
+        first = None
     else:
         length = len(sentence)
         first = sentence[0]
