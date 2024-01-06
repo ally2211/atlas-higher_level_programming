60abffb6e912728d1229d890ba81e8c6f1cb5bc6
ally2211 <amendieta.va@gmail.com>
2024-01-06 07:55:17 -0600

    mult

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -7,8 +7,8 @@ def best_score(a_dictionary):
 
     max_score = None
     max_key = None
-    for key,value in a_dictionary.items():
-        if  max_score is None or value > max_score:
+    for key, value in a_dictionary.items():
+        if max_score is None or value > max_score:
             max_key = key
             max_score = value
     return max_key
---
+++ python-more_data_structures/11-multiply_list_map.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def multiply_list_map(my_list=[], number=0):
+    if my_list is None:
+        return
+
+    new_list = []
+    for element in my_list:
+        new_list.append(element * number)
+
+    return new_list
