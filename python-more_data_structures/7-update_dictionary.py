fcd7794469cad7c7ada76a1c2776719faf6d9728
ally2211 <amendieta.va@gmail.com>
2024-01-06 04:26:46 -0600

    dict

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -2,10 +2,7 @@
 
 
 def update_dictionary(a_dictionary, key, value):
-    if a_dictionary is None
+    if a_dictionary is None:
         return
 
-    if key in a_dictionary:
-        a_dictionary[key] = value
-    else:
-        a_dictionary[key] = value
+    a_dictionary[key] = value
