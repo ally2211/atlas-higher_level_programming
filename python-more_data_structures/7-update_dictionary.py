581898d03423262d62278792d5c31c514c76fff6
ally2211 <amendieta.va@gmail.com>
2024-01-06 04:06:30 -0600

    dict

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def update_dictionary(a_dictionary, key, value):
+    if a_dictionary is None
+        return
+
+    if key in a_dictionary:
+        a_dictionary[key] = value
+    else:
+        a_dictionary[key] = value
