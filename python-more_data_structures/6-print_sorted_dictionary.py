d94b77a7b994cc246b4056a8830c3765be630bc2
ally2211 <amendieta.va@gmail.com>
2024-01-06 03:49:51 -0600

    dict

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,12 +3,12 @@
 
 def print_sorted_dictionary(a_dictionary):
     if a_dictionary is None:
-        return 1 
+        return 1
 
     sorted_keys = sorted(a_dictionary)
     sorted_dict = {}
     for key in sorted_keys:
         sorted_dict[key] = a_dictionary[key]
-    
+ 
     for key, value in sorted_dict.items():
         print(f"{key}: {value}")
