7e6e7ac210a384d1df648d61991d2163643879ae
ally2211 <amendieta.va@gmail.com>
2024-01-06 10:30:35 -0600

    map

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -2,12 +2,4 @@
 
 
 def multiply_list_map(my_list=[], number=0):
-    if my_list is None:
-        return
-    """
-    new_list = []
-    for element in my_list:
-        new_list.append(element * number)
-    return new_list
-    """
     return list(map(lambda x: x * number, my_list))
