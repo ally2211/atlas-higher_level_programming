2ecf1b3238c760f8cfc47977bb40cccae83837cd
ally2211 <amendieta.va@gmail.com>
2024-01-06 21:49:55 -0600

    div

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def safe_print_division(a, b):
+    result = None
+    try:
+        result = a / b
+    except (TypeError, ValueError, ZeroDivisionError):
+        result = None
+    finally:
+        if result is None:
+            print("Inside rsult: None")
+        else:
+            print("Inside result: {:.1f}".format(result))
+    return result
