88dcf381ca01421bf57080bb4b2fb2aec61bc831
ally2211 <amendieta.va@gmail.com>
2024-01-06 23:11:36 -0600

    exception

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -7,7 +7,7 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             division_result = my_list_1[i] / my_list_2[i]
         except (TypeError):
-            print ("wrong type")
+            print("wrong type")
             division_result = 0
         except (ZeroDivisionError):
             print("division by 0")
