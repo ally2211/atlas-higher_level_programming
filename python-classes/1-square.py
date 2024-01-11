3f10b6d5a5b56cd5af7795dced50aaf9ee209489
ally2211 <amendieta.va@gmail.com>
2024-01-11 05:48:30 -0600

    sq

---
+++ python-classes/1-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This is a module that defines a class Square.
+It has a mismatched attribute of size.
+"""
+
+
+class Square:
+    """
+    Class documentation:  this class initializes instance of size
+    but mismatches with __size
+    """
+
+    def __init__(self, size):
+        """
+        Initialize Square instance with size.
+        """
+        self.__size = size
+
+
+if __name__ == "__Square__":
+    Square()
