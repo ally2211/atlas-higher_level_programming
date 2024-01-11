c51d38b7cc5f63abbb5571beda4817547fc43f55
ally2211 <amendieta.va@gmail.com>
2024-01-11 05:23:50 -0600

    square

---
+++ python-classes/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This is a module that defines a class Square.
+Yet it does not have any attributes or functions.
+"""
+
+
+class Square:
+    """
+    Class documentation: This is and empty square
+    """
+    pass
