e2f7b8efd3f533ebe76fbb022dd7f7ed41888a8e
ally2211 <amendieta.va@gmail.com>
2024-01-14 16:40:15 -0600

    NaN

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -10,9 +10,9 @@ def add_integer(a, b=98):
     Return the sum of a and b.
     """
 
-    if not isinstance(a, (float, int)) or a is None:
+    if not isinstance(a, (float, int)) or a is None or a != a:
         raise TypeError("a must be an integer")
-    if not isinstance(b, (float, int)) or b is None:
+    if not isinstance(b, (float, int)) or b is None or b != b:
         raise TypeError("b must be an integer")
 
     return int(a) + int(b)
