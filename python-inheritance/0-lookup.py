1ec326e0d2411fdb0ec40229a92b72f04af38aa9
ally2211 <amendieta.va@gmail.com>
2024-01-19 01:13:26 -0600

    lookup

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module documentation:  This help to get a handle
+on what methods are available to your object
+"""
+
+
+def lookup(obj):
+    """
+    Function Description:  Returns methods of a list object
+    """
+    return dir(obj)
