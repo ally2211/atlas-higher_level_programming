52963d6b0e7b6bf5fae830556c5b2d39ab126410
ally2211 <amendieta.va@gmail.com>
2024-01-24 11:50:44 -0600

    inherit

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+class doc:  determine if same object
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    function to check if an object is an instance of,
+    or if the object is an instance of a class that inherited from,
+    the specified class
+    """
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
