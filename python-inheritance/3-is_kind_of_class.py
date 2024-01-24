a42f581ceaa400b94b05f719fd77e6b3a4c1ab16
ally2211 <amendieta.va@gmail.com>
2024-01-24 11:28:50 -0600

    test isinstance

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+class doc:  determine if same object
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    function to check if an object is an instance of, or if the object is an instance of a class that inherited from, the specified class
+    """
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
