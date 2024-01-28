a7dd56c0c0e41c2ce994c8de18319271e6b2506b
ally2211 <amendieta.va@gmail.com>
2024-01-28 03:53:55 -0600

    update to check type of class

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -8,9 +8,9 @@ def inherits_from(obj, a_class):
     """
     function to check if an object is an instance of,
     or if the object is an instance of a class that inherited from,
-    the specified class
+    the specified class.  Therefore given a=True, it would return False.
     """
-    if issubclass(type(obj), a_class):
+    if issubclass(type(obj), a_class) and not type(obj) is a_class:
         return True
     else:
         return False
