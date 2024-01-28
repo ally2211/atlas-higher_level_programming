f69941c0d1c39503a3f4c0da6cc1b00de9758d81
ally2211 <amendieta.va@gmail.com>
2024-01-28 00:08:29 -0600

    valueerror

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -18,11 +18,11 @@ class Rectangle(BaseGeometry):
         """
         if not isinstance(height, int):
             raise TypeError("height must be an integer")
-        if width <= 0:
+        if height <= 0:
             raise ValueError("height must be greater than 0")
         if not isinstance(width, int):
             raise TypeError("width must be an integer")
-        if height <= 0:
+        if width <= 0:
             raise ValueError("width must be greater than 0")
         self.__width = width
         self.__height = height
