7bc2d49a64bcd47a1dc4314c559efb8bf2092f09
ally2211 <amendieta.va@gmail.com>
2024-01-27 19:52:00 -0600

    non implemented error

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+class doc:  determine if same object
+"""
+
+
+class BaseGeometry:
+    """
+    Class doc:  Empty BaseGeometry class
+    """
+    def __init__(self):
+        pass
+
+    def area(self):
+        """
+        area: method to calculate area
+        """
+        raise Exception("area() is not implemented")
