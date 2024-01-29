ed83dbd008b691f842fa905db3087e3856beefd9
ally2211 <amendieta.va@gmail.com>
2024-01-28 19:12:13 -0600

    remove os

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -9,9 +9,6 @@ def read_file(filename=""):
     Function Doc: read a file
     """
     with open(filename, "r", encoding="utf-8") as f:
-        # Check if the file is empty
-        if os.path.getsize(filename) == 0:
-            print("File is empty.")
-        else:
             for line in f:
                 print(line, end='')
+
