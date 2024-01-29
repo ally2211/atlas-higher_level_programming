01336847fae3d9333f57422f8aa57a39933a846a
ally2211 <amendieta.va@gmail.com>
2024-01-29 02:34:59 -0600

    pycode

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -6,7 +6,7 @@ Module Doc:  returns pascal triangle
 
 def pascal_triangle(n):
     """
-    Returns a list of lists of integers 
+    Returns a list of lists of integers
     representing the Pascal's triangle of n.
     Returns an empty list if n <= 0.
     """
@@ -17,7 +17,8 @@ def pascal_triangle(n):
 
     for i in range(1, n):
         row = [1]  # First element of each row is always 1
-        # Each element in the middle of the row is the sum of the two elements above it
+        # Each element in the middle of the row is the
+        # sum of the two elements above it
         for j in range(1, i):
             row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
         row.append(1)  # Last element of each row is always 1
