b86e680310b97f506bf336d6ee5ba0c30e4ed03a
ally2211 <amendieta.va@gmail.com>
2024-01-29 01:40:01 -0600

    student class and dictionary

---
+++ python-input_output/9-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Module Doc:  returns dictionary of student
+"""
+
+
+class Student:
+    """
+    Class doc that defines a student
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialize a Student instance.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Retrieves a dictionary representation of a Student instance.
+        """
+        return {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'age': self.age
+        }
+
+# Example usage
+student = Student("John", "Doe", 25)
+student_json = student.to_json()
+print(student_json)
