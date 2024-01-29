8d30f2680fd9f23c7b2be05e30cb4601416eecd9
ally2211 <amendieta.va@gmail.com>
2024-01-28 19:13:51 -0600

    indentations

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -9,6 +9,5 @@ def read_file(filename=""):
     Function Doc: read a file
     """
     with open(filename, "r", encoding="utf-8") as f:
-            for line in f:
-                print(line, end='')
-
+        for line in f:
+            print(line, end='')
