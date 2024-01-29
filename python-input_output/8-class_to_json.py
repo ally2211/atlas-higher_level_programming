8efedaa1fb034caeb3997b3ceb6a9354d49238c3
ally2211 <amendieta.va@gmail.com>
2024-01-29 01:33:43 -0600

    pycode

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -6,10 +6,10 @@ Module Doc:  given an instant of a class return dictionary
 
 def class_to_json(obj):
     """
-    Write a function that returns 
-    the dictionary description 
-    with simple data structure 
-    (list, dictionary, string, integer and boolean) 
+    Write a function that returns
+    the dictionary description
+    with simple data structure
+    (list, dictionary, string, integer and boolean)
     for JSON serialization of an object:
     """
     serializable_types = (list, dict, str, int, bool)
@@ -17,7 +17,8 @@ def class_to_json(obj):
     for attr in dir(obj):
         # Get the attribute's value
         value = getattr(obj, attr)
-        # Check if the attribute is serializable and not a built-in attribute or method
+        # Check if the attribute is serializable
+        # and not a built-in attribute or method
         if isinstance(value, serializable_types) and not attr.startswith("__"):
             obj_dict[attr] = value
     return obj_dict
