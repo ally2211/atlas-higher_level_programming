0374c4a3b6a77171b419d33edd953bdfffbf2940
ally2211 <amendieta.va@gmail.com>
2024-01-29 02:54:54 -0600

    add error instead

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -11,6 +11,7 @@ def to_json_string(my_obj):
     """
     # Convert set to list
     if isinstance(my_obj, set):
-        my_obj = list(my_obj)
+        #my_obj = list(my_obj)
+        print("[TypeError] Object of type set is not JSON serializable")
     json.dumps(my_obj)
     return json.dumps(my_obj)
