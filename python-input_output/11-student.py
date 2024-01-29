0d10e25c1846e081fff2d6a9629d1e0fd491786c
ally2211 <amendieta.va@gmail.com>
2024-01-29 02:19:47 -0600

    pep

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -46,7 +46,8 @@ class Student:
 
     def reload_from_json(self, json):
         """
-        Replaces all attributes of the Student instance with values from the json dictionary.
+        Replaces all attributes of the Student instance
+        with values from the json dictionary.
         """
         for key, value in json.items():
             if hasattr(self, key):
