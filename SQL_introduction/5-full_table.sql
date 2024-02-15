3261e19fec68f3509cf7f4f20da632213390d99d
ally2211 <amendieta.va@gmail.com>
2024-02-15 15:27:15 -0600

    get description

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,11 +1,17 @@
--- Retrieve table information from the INFORMATION_SCHEMA database
-
+SET @db_name = 'hbtn_0c_0';
 SET @table_name = 'first_table';
 
 -- Generate CREATE TABLE statement for the specified table
-SELECT @table_name AS 'Table',
-CONCAT('CREATE TABLE `first_table` (\n',
-GROUP_CONCAT(CONCAT('  `', COLUMN_NAME, '` ', COLUMN_TYPE, IF(IS_NULLABLE = 'YES', ' DEFAULT NULL', ''), '\n')),
-') ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci') AS 'Create Table'
+SELECT CONCAT('CREATE TABLE `', @table_name, '` (\n',
+  GROUP_CONCAT(
+    CONCAT('  `', COLUMN_NAME, '` ', COLUMN_TYPE,
+      IF(IS_NULLABLE = 'YES', ' DEFAULT NULL', ' NOT NULL'),
+      IF(COLUMN_DEFAULT IS NOT NULL, CONCAT(' DEFAULT \'', COLUMN_DEFAULT, '\''), ''),
+      IF(EXTRA != '', CONCAT(' ', EXTRA), ''),
+      '\n'
+    ) ORDER BY ORDINAL_POSITION ASC SEPARATOR ','),
+  ') ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;') AS CreateTableStatement
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = @table_name
+WHERE TABLE_SCHEMA = @db_name AND TABLE_NAME = @table_name
+GROUP BY TABLE_NAME;
+
