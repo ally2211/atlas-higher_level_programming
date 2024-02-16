9b456c67c0e0808df379b9d27da806f4614d8ec1
ally2211 <amendieta.va@gmail.com>
2024-02-15 21:46:22 -0600

    give permissions

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,13 +1,15 @@
 SELECT COUNT(*) INTO @user_exists FROM mysql.user WHERE user = 'user_0d_1';
 
 -- Create the user if it does not exist
-SET @create_user_query = IF(@user_exists = 0, 'CREATE USER \'user_0d_1\'@\'localhost\' IDENTIFIED BY \'user_0d_1_pwd\';', 'SELECT \'User already exists\' status;');
+-- User already exists
+SET @create_user_query = IF(@user_exists = 0, 'CREATE USER \'user_0d_1\'@\'localhost\' IDENTIFIED BY \'user_0d_1_pwd\';', 'SELECT status;');
 PREPARE stmt FROM @create_user_query;
 EXECUTE stmt;
 DEALLOCATE PREPARE stmt;
 
 -- Grant privileges only if the user was just created
-SET @grant_privileges_query = IF(@user_exists = 0, 'GRANT ALL PRIVILEGES ON *.* TO \'user_0d_1\'@\'localhost\';', 'SELECT \'User already exists, no privileges granted\' status;');
+-- User already exists, no privileges granted
+SET @grant_privileges_query = IF(@user_exists = 0, 'GRANT ALL PRIVILEGES ON *.* TO \'user_0d_1\'@\'localhost\';', 'SELECT status;');
 PREPARE stmt FROM @grant_privileges_query;
 EXECUTE stmt;
 DEALLOCATE PREPARE stmt;
