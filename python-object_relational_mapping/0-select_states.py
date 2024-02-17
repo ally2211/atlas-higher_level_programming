d0f731639f67caec4ad4738348dc1e724afb05ab
ally2211 <amendieta.va@gmail.com>
2024-02-16 18:46:16 -0600

    trailing whitespace

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -6,15 +6,16 @@ A script that lists all states from the database hbtn_0e_0_usa.
 import MySQLdb
 import sys
 
+
 def list_states(username, password, dbname):
     """
     Lists all states from the database.
     """
     db = MySQLdb.connect(host="localhost",
-                        port=3306,
-                        user=sys.argv[1],
-                        passwd=sys.argv[2],
-                        db=sys.argv[3])
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
 
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
@@ -23,6 +24,7 @@ def list_states(username, password, dbname):
         print(row)
     cur.close()
     db.close()
+
 
 if __name__ == "__main__":
     if len(sys.argv) == 4:
