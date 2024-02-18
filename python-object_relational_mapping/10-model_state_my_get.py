da9d23f21effc48c9e41ec256ecc327bc9e9dd9b
ally2211 <amendieta.va@gmail.com>
2024-02-17 23:41:02 -0600

    print state.id when passing an arg of state

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+A script that lists all states from the database hbtn_0e_0_usa.
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+def list_states(username, password, dbname, state):
+    """
+    Lists all states from the database.
+    """
+
+    # Construct the connection string
+    cstring = f'mysql+pymysql://{username}:{password}@localhost/{dbname}'
+
+    # Create the engine
+    engine = create_engine(cstring)
+
+    # Bind the engine to the Base metadata
+    Base.metadata.bind = engine
+
+    # Create a Session
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+
+    # Query all states
+    states = session.query(State)\
+        .filter(State.name.like(f'%{state}%'))\
+        .order_by(State.id)\
+        .all()
+
+    # Print states
+    if states:
+        for state in states:
+            print(f"{state.id}")
+    else:
+        print("Not found")
+
+    # Close the session
+    session.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 5:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        dbname = sys.argv[3]
+        state = sys.argv[4]
+        list_states(username, password, dbname, state)
