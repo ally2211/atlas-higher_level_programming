5c4bcb97bb2daba582f95beb2f66c8c6c7c902d3
ally2211 <amendieta.va@gmail.com>
2024-02-17 21:29:34 -0600

    first state select only

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+A script that lists all State objects from the database hbtn_0e_6_usa
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+def list_states(username, password, dbname):
+    """
+    Connects to a database and lists all State objects from the database.
+    """
+
+    # Construct the connection string
+    cstring = f'mysql+pymysql://{username}:{password}@localhost/{dbname}'
+
+    # Create the engine
+    engine = create_engine(cstring)
+
+    # Bind the engine to the Base metadata
+    Base.metadata.bind = engine
+
+    # Create a Session
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+
+    # Query first state
+    first_state = session.query(State).order_by(State.id).first()
+
+    # Print first state
+    print(f"{first_state.id}: {first_state.name}")
+
+    # Close the session
+    session.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 4:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        dbname = sys.argv[3]
+        list_states(username, password, dbname)
+    else:
+        print("Usage: ./list_states.py <mysql username>"
+              "<mysql password> <database name>")
