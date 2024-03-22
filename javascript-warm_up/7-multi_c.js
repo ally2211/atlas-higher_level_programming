08bfc3564acd5b76f530c58b0c78b3a7d1d5a522
ally2211 <amendieta.va@gmail.com>
2024-03-22 15:39:32 -0500

    pass arg number or console repeats

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+//  Write a script that prints 3 lines:
+
+const number = parseInt(process.argv[2]);
+
+if (isNaN(number)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < number; i++) {
+    console.log('C is fun');
+  }
+}
