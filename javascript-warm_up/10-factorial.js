083d6f8337b3ffba94e238bb36e9c37ee4e7d246
ally2211 <amendieta.va@gmail.com>
2024-03-22 16:48:41 -0500

    factorial

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+//  script that computes and prints a factorial
+
+function factorial (n) {
+  if (n === 0 || n === 1) {
+    return 1; // Base case: factorial of 0 or 1 is 1
+  } else {
+    return n * factorial(n - 1); // Recursive case: n! = n * (n-1)!
+  }
+}
+
+const number = parseInt(process.argv[2], 10);
+
+if (isNaN(number)) {
+    console.log(`${factorial(0)}`);
+}
+
+console.log(`${factorial(number)}`);
