af1661eaecf058db95f938857d23429bafb07e4e
ally2211 <amendieta.va@gmail.com>
2024-03-22 13:55:23 -0500

    semistandard

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
-// Subtracting the first two elements
-// which are the path and file
-
-let counter = 0;
-
+// print first argument only
 
 if (process.argv[2]) {
   console.log(process.argv[2]);
 } else if (!process.argv[2]) {
-  console.log('No Argument');
+  console.log('No argument');
 }
