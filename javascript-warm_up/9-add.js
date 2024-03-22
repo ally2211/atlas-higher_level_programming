5031d397a371846e605d4fc8b5efaa438884e3f5
ally2211 <amendieta.va@gmail.com>
2024-03-22 16:07:58 -0500

    add function

---
+++ javascript-warm_up/9-add.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+//  Write a script that prints 3 lines:
+
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+
+function add (a, b) {
+  if (!isNaN(a) & !isNaN(b)) {
+    console.log(a + b);
+  }
+}
+add(a, b);
