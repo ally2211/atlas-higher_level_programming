f01debd5112aedb76ca1d92cf335b1fe21563d0b
ally2211 <amendieta.va@gmail.com>
2024-03-23 01:45:06 -0500

    reverse list

---
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+exports.esrever = function (list) {
+  const j = list.length;
+  const listreverse = [j];
+  for (let i = 0; i < list.length; i++) {
+    listreverse[j - i - 1] = list[i];
+  }
+  return listreverse;
+};
