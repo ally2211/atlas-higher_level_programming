3619eaa6067278f89b5171a8c58ff5ea70d821dd
ally2211 <amendieta.va@gmail.com>
2024-03-23 00:29:29 -0500

    set height to width for square

---
+++ javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,38 @@
+#!/usr/bin/node
+//  print() that prints the rectangle using the character X
+class Rectangle {
+  constructor (w) {
+    if (Number.isInteger(w) && w > 0) {
+      this.width = w;
+      this.height = w;
+    }
+  }
+
+  print () {
+    if (this.width === 0 || this.height === 0) {
+      console.log('Missing size');
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        let row = ''; // Start with an empty string for the row
+        for (let j = 0; j < this.width; j++) {
+          row += 'X'; // Add 'X' to the row string for each column
+        }
+        console.log(row);
+      }
+    }
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+}
+// Exporting the class if you're using Node.js
+// and want it to be accessible from other files
+module.exports = Rectangle;
