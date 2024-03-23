eef241a5529dbff51fafa6a8c085967b6a706152
ally2211 <amendieta.va@gmail.com>
2024-03-22 19:59:20 -0500

    remove extra print

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -7,16 +7,15 @@ const numberOfArguments = process.argv.length - 2;
 if (numberOfArguments === 0 || numberOfArguments === 1) {
   console.log(0);
 } else {
-  let myArray = [];
+  const myArray = [];
   for (let i = 0; i < numberOfArguments; i++) {
     myArray[i] = process.argv[i + 2];
-    console.log(myArray[i]);
-}
-// Sort the array in ascending order and then reverse it to make it descending
-myArray.sort((a, b) => a - b).reverse();
+  }
+  // Sort the array in ascending order and then reverse it to make it descending
+  myArray.sort((a, b) => a - b).reverse();
 
-// Check if there are at least two unique elements
-const uniqueElements = [...new Set(myArray)];
-console.log(uniqueElements[1]);
+  // Check if there are at least two unique elements
+  const uniqueElements = [...new Set(myArray)];
+  console.log(uniqueElements[1]);
 // Print the second highest element
 }
