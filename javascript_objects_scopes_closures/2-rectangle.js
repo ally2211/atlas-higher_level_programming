87e39d51457edee1373d20d8690174018019910f
ally2211 <amendieta.va@gmail.com>
2024-03-22 22:44:34 -0500

    semistandard

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -4,7 +4,7 @@ class Rectangle {
     if (Number.isInteger(w) && Number.isInteger(h) && w > 0 && h > 0) {
       this.width = w;
       this.height = h;
-    } 
+    }
   }
 }
 // Exporting the class if you're using Node.js
