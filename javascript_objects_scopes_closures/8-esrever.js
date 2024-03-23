6f78f27bcd65c7d66cff11a54b688a1fa5e4a5a0
ally2211 <amendieta.va@gmail.com>
2024-03-23 01:49:10 -0500

    empty list scenario

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
   const j = list.length;
-  const listreverse = [j];
+  const listreverse = [];
   for (let i = 0; i < list.length; i++) {
     listreverse[j - i - 1] = list[i];
   }
