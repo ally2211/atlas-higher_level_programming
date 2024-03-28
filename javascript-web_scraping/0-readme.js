e17552d0151511f12fd089b3edb6733e90a419a6
ally2211 <amendieta.va@gmail.com>
2024-03-28 13:21:10 -0500

    semistandard

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -3,14 +3,14 @@ const fs = require('fs');
 const path = process.argv[2];
 
 if (!path) {
-    //console.log('Please provide a file path as an argument.');
-    process.exit(1);
+  // console.log('Please provide a file path as an argument.');
+  process.exit(1);
 }
 
 fs.readFile(path, { encoding: 'utf-8' }, (err, data) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    console.log(data);
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
 });
