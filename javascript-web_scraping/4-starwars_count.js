2e7f93278f2f9eebfc3f57bd5be6222f95b7511e
ally2211 <amendieta.va@gmail.com>
2024-03-29 01:15:00 -0500

    count films

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+const request = require('request');
+const charID = 18;
+
+if (!charID) {
+  console.log('Please provide a movie ID as an argument.');
+  process.exit(1);
+}
+
+const url = `https://swapi-api.hbtn.io/api/people/${charID}/`;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+  try {
+    const data = JSON.parse(body);
+    // Convert the JSON response into an object
+    const numberOfFilms = data.films.length;
+    console.log(numberOfFilms);
+    // console.log(data.films); // Print the title of the movie
+  } catch (parseError) {
+    console.error('Error parsing JSON:', parseError);
+  }
+});
