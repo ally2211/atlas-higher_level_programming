e6e06d1f97acc1c1358b44177c08ebaa10ca0f68
ally2211 <amendieta.va@gmail.com>
2024-03-29 15:09:12 -0500

    written with request instead

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,38 +1,28 @@
 #!/usr/bin/node
-const http = require('http');
+const request = require('request');
 const fs = require('fs');
 const webpage = process.argv[2];
-const filewrite = process.argv[3];
+const filePath = process.argv[3];
 
-if (!filewrite) {
-  // console.log('Please provide a file path as an argument.');
-  process.exit(1);
-}
-if (!webpage) {
-  console.log('Please provide a webpage URL as the first argument.');
+if (!webpage || !filePath) {
+  console.log('Usage: <script> <URL> <file path>');
   process.exit(1);
 }
 
-// clear the file
-fs.writeFileSync(filewrite, '');
+request(webpage, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
 
-http.get(webpage, (response) => {
-    let data = '';
-    // A chunk of data has been received.
-    response.on('data', (chunk) => {
-      data += chunk;
+  if (response && response.statusCode === 200) {
+    fs.writeFile(filePath, body, 'utf8', (err) => {
+      if (err) {
+        //Failed to write file:
+        console.error(err);
+      }
     });
-  
-    // The whole response has been received. Print out the result.
-    response.on('end', () => {
-      console.log(data);
-      fs.writeFile(filewrite, data, 'utf8', (err) => {
-        if (err) {
-          console.error(err);
-        }
-        // console.log('File has been written successfully');
-      });
-    });
-  }).on('error', (err) => {
-    console.log('Error: ' + err.message);
-  });
+  } else {
+    console.log('Request failed with status:', response && response.statusCode);
+  }
+});
---
+++ javascript-web_scraping/backuprequest.js
@@ -0,0 +1,38 @@
+#!/usr/bin/node
+const http = require('http');
+const fs = require('fs');
+const webpage = process.argv[2];
+const filewrite = process.argv[3];
+
+if (!filewrite) {
+  // console.log('Please provide a file path as an argument.');
+  process.exit(1);
+}
+if (!webpage) {
+  console.log('Please provide a webpage URL as the first argument.');
+  process.exit(1);
+}
+
+// clear the file
+fs.writeFileSync(filewrite, '');
+
+http.get(webpage, (response) => {
+    let data = '';
+    // A chunk of data has been received.
+    response.on('data', (chunk) => {
+      data += chunk;
+    });
+  
+    // The whole response has been received. Print out the result.
+    response.on('end', () => {
+      console.log(data);
+      fs.writeFile(filewrite, data, 'utf8', (err) => {
+        if (err) {
+          console.error(err);
+        }
+        // console.log('File has been written successfully');
+      });
+    });
+  }).on('error', (err) => {
+    console.log('Error: ' + err.message);
+  });
