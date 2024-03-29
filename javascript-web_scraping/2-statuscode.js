9b11ae7275e33542be08e5e66624466deb6effb6
ally2211 <amendieta.va@gmail.com>
2024-03-28 22:29:36 -0500

    semistandard

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -8,10 +8,9 @@ if (!urlpath) {
 }
 
 request(urlpath, (error, response, body) => {
-    if (error) {
-      console.error(error);
-      return;
-    }
-    console.log('code: ', response && response.statusCode);
+  if (error) {
+    console.error(error);
+    return;
+  }
+  console.log('code:', response && response.statusCode);
 });
-
