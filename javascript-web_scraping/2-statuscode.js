3398b0dfc05b2186af4459961201e93aee161890
ally2211 <amendieta.va@gmail.com>
2024-03-28 22:26:01 -0500

    request get URL statusCode

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const request = require('request');
+const urlpath = process.argv[2];
+
+if (!urlpath) {
+  // console.log('Please provide a URL path as an argument.');
+  process.exit(1);
+}
+
+request(urlpath, (error, response, body) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+    console.log('code: ', response && response.statusCode);
+});
+
