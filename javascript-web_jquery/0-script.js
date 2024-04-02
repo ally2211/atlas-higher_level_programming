c2a002abb0f0b74c4770ab0652541f89c86b574a
ally2211 <amendieta.va@gmail.com>
2024-04-02 18:21:24 -0500

    change header color

---
+++ javascript-web_jquery/0-script.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// Use document.querySelector to select the <header> element
+const headerElement = document.querySelector('header');
+
+// Set the text color of the <header> element to red (#FF0000)
+headerElement.style.color = '#FF0000';
