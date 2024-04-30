f19b1935ab59eb3f0e103b6fcbf845395854a10d
ally2211 <amendieta.va@gmail.com>
2024-04-30 10:05:19 -0500

    Delete python-if_else_loops_functions directory

--- python-if_else_loops_functions/3-print_alphabt.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-output = ""
-for letter in range(ord('a'), ord('z') + 1):
-    if chr(letter) not in ['e', 'q']:
-        output += "{}".format(chr(letter))
-
-print(output, end='')
