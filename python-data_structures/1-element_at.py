aed7ef42c0e80693b6d3acc9f4a3924547731e39
ally2211 <amendieta.va@gmail.com>
2024-05-02 20:25:23 -0500

    index get from list

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def element_at(my_list, idx):
+    lenlist = len(my_list)
+    if (idx >= 0):
+        if idx < lenlist:
+            return (my_list[idx])
+        else:
+            return ("None")
+    else:
+        return ("None")
